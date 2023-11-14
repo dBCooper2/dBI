f4a9899e80c90b4251abfdb81a5c4c5282d6f1a0
dBCooper2 <t.rowland711@gmail.com>
2023-11-14 13:57:09 -0600

    added skeletons, finish db first

---
+++ code/v5/analysis_functions.py
@@ -0,0 +1,22 @@
+# TODO: ADD FUNCTIONS HERE:
+# All of these will take in dataframe objects and forecast with those, read into how to perform these operations!
+
+
+def moving_avg():
+    pass
+
+def linear_reg():
+    pass
+
+def exp_smoothing():
+    pass
+
+def linear_opt():
+    pass
+
+def int_opt():
+    pass
+
+# Should I add these to Portfolio instead?
+def stock_analysis():pass
+def portfolio_analysis():pass
